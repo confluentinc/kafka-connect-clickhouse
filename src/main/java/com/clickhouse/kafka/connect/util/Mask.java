a0744d7cb1c22a18271bdc7b2ec8696022fe7ea3
pranav <pragupta@confluent.io>
2025-12-18 11:08:18 +0530

    update password mask

--- src/main/java/com/clickhouse/kafka/connect/util/Mask.java
+++ src/main/java/com/clickhouse/kafka/connect/util/Mask.java
@@ -2,11 +2,9 @@ package com.clickhouse.kafka.connect.util;
 
 public class Mask {
 
+    private static final String PASSWORD_MASK = "********";
+
     public static String passwordMask(String password) {
-        if (password.length() <= 6) {
-            return "*".repeat(password.length());
-        }
-        String tmpPassword = "***" + password.substring(3, password.length() - 3) + "***";
-        return tmpPassword;
+        return PASSWORD_MASK;
     }
 }
